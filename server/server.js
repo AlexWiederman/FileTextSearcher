aae92ca207b4c194a65606fafa606ead1ae12a5e
Alex Wiederman <amwiederman15@gmail.com>
2023-05-04 14:27:18 -0400

    Added serveral functions but only one works with file name

--- server/server.js
+++ server/server.js
@@ -53,11 +53,58 @@ const fs = require('fs');
 app.get('/search', (req, res) => {
 
   const query = req.query.q;
-  console.log(__dirname)
-  let location = "C:\Alex_W\Coding\KnowledgebaseExample"
+  // console.log(__dirname)
+  // let location = "C:\ExampleKnowledgebase"
+  let location = "C:\\ExampleKnowledgebase"
+  // const query = "Test1.txt"
+
   const results = searchFiles(query, location);
+
   res.send(results);
 });
+
+// function searchFiles( searchText, dir) {
+//   // console.log(`Search: ${searchText} Dir: ${dir}`)
+//   const results = [];
+//   fs.readdir(dir, (err, files) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     files.forEach((file) => {
+//       const filePath = path.join(dir, file);
+
+//       fs.stat(filePath, (err, stat) => {
+//         if (err) {
+//           console.error(err);
+//           return;
+//         }
+
+//         if (stat.isDirectory()) {
+//           searchFiles(filePath, searchText);
+//         } else {
+//           fs.readFile(filePath, 'utf8', (err, data) => {
+//             if (err) {
+//               console.error(err);
+//               return;
+//             }
+
+//             if (data.indexOf(searchText) !== -1) {
+//               results = `Found '${searchText}' in file '${filePath}'`
+//               return results
+//               // console.log(`Found '${searchText}' in file '${filePath}'`);
+
+//             }
+//           });
+//         }
+//       });
+//     });
+//   });
+// }
+
+
+//Text found in file name WORKS
 
 function searchFiles(query, dir) {
   const files = fs.readdirSync(dir);
@@ -76,6 +123,65 @@ function searchFiles(query, dir) {
 
   return results;
 }
+
+// function searchFiles(dir, searchText, results) {
+//   results = results || [];
+
+//   fs.readdir(dir, (err, files) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//     files.forEach((file) => {
+//       const filePath = path.join(dir, file);
+
+//       fs.stat(filePath, (err, stat) => {
+//         if (err) {
+//           console.error(err);
+//           return;
+//         }
+
+//         if (stat.isDirectory()) {
+//           searchFiles(filePath, searchText, results);
+//         } else {
+//           fs.readFile(filePath, 'utf8', (err, data) => {
+//             if (err) {
+//               console.error(err);
+//               return;
+//             }
+
+//             if (data.indexOf(searchText) !== -1) {
+//               console.log(`Found '${searchText}' in file '${filePath}'`);
+//               results.push(filePath);
+//             }
+//           });
+//         }
+//       });
+//     });
+//   });
+// }
+
+
+// function searchFiles(query, location) {
+//   const results = [];
+// console.log(`Test1 ${location}`)
+//   fs.readdirSync(query).forEach((file) => {
+//     const filePath = path.join(query, file);
+
+//     if (fs.statSync(filePath).isDirectory()) {
+//       results.push(...searchFiles(filePath, location));
+//     } else {
+//       const fileContent = fs.readFileSync(filePath, 'utf8');
+//       if (fileContent.includes(location)) {
+//         results.push(filePath);
+//       }
+//     }
+//   });
+
+//   return results;
+// }
+
 
 
 // Call the async function to start the server
